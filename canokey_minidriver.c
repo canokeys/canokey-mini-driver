e75b1642ecd61b4f7722312e2f8bf26e11cb0e5c
Shengqi Chen <harry-chen@outlook.com>
2025-03-11 14:31:14 +0800

    Use LF and 2-space indent for all files
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- canokey_minidriver.c
+++ canokey_minidriver.c
@@ -30,7 +30,8 @@ PFN_CSP_UNPAD_DATA g_pfnCspUnpadData = NULL;
 
 // DllMain function
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-
+  
+  
   int res = cmd_init_logging("C:\\Logs\\canokey_minidriver.log", CMD_LOG_LEVEL_DEBUG);
   if (res != 0) {
 	  return res;
